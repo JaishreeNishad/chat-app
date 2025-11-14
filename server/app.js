e750740c7789ddeed86b053603da4c3fc0bd67ee
Jaishree Nishad <120036134+JaishreeNishad@users.noreply.github.com>
2025-11-14 14:37:37 +0530

    Create message api

--- server/app.js
+++ server/app.js
@@ -8,7 +8,8 @@ require("./dp/connection");
 
 //import files
 const Users = require("./models/Users");
-const Conversations = require("./models/Conversations"); // Assuming this is your Mongoose model
+const Conversations = require("./models/Conversations");
+const Messages = require("./models/Messages"); // Assuming this is your Mongoose model
 
 // Middleware for parsing request bodies
 app.use(express.urlencoded({ extended: false }));
@@ -152,6 +153,71 @@ app.get("/api/conversation/:userId", async (req, res) => {
   } catch (error) {
     console.log("Error fetching conversations:", error);
     res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/api/message", async (req, res) => {
+  try {
+    const { conversationId, senderId, message, receiverId = "" } = req.body;
+    if (!senderId || !message)
+      return res.status(400).send("please fill all required field");
+    if (!conversationId && receiverId) {
+      const newConversation = new Conversations({
+        members: [senderId, receiverId],
+      });
+      await newConversation.save();
+      const newMessage = new Messages({
+        conversationId: newConversation._id,
+        senderId,
+        message,
+      });
+      await newMessage.save();
+      return res.status(200).send("message sent successfully");
+    } else if (!conversationId && !receiverId) {
+      return res.status(400).send("please fill all required filed");
+    }
+    const newMessage = new Messages({ conversationId, senderId, message });
+    await newMessage.save();
+    res.status(200).send("Message send successfully");
+  } catch (error) {
+    console.log(error, "error");
+  }
+});
+
+app.get("/api/message/:conversationId", async (req, res) => {
+  try {
+    const conversationId = req.params.conversationId;
+    if (!conversationId == "new") return res.status(200).json([]);
+    const messages = await Messages.find({ conversationId });
+    const messageUserData = Promise.all(
+      messages.map(async (message) => {
+        const user = await Users.findById(message.senderId);
+        return {
+          user: { email: user.email, fullName: user.fullName },
+          message: message.message,
+        };
+      })
+    );
+    res.status(200).json(await messageUserData);
+  } catch (error) {
+    console.log(error, "error");
+  }
+});
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await Users.find();
+    const userData = Promise.all(
+      users.map(async (user) => {
+        return {
+          user: { email: user.email, fullName: user.fullName },
+          userId: user._id,
+        };
+      })
+    );
+    res.status(200).json(await userData);
+  } catch (error) {
+    console.log(error, "error");
   }
 });
 
